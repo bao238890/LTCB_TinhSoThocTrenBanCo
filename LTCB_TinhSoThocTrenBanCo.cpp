d317c4e25a6bea98b39a0ce5008d8cd4c94c94fd
Hoangsharksuy <hduyshark@gmail.com>
2024-03-04 16:22:08 +0700

    Add project files.

---
+++ LTCB_TinhSoThocTrenBanCo.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+	int soO, sothoc=0;
+	cout << "Nhap so o:"; cin >> soO;
+	for (int i = 0; i < soO; i++) {
+		cout << "O so" << i << "Co so thoc la" <<pow(2,1) << endl;
+		sothoc += pow(2,i);
+	}
+	cout << "Tong so thoc chua trong" << soO << "la" << sothoc << "hat\n";
+	return 0;
+}
