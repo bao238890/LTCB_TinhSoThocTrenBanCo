43d887ecd0a9f18670a97d13e66e8d517575824b
bao238890 <160477993+bao238890@users.noreply.github.com>
2024-03-04 16:24:35 +0700

    Update LTCB_TinhSoThocTrenBanCo.cpp

--- LTCB_TinhSoThocTrenBanCo.cpp
+++ LTCB_TinhSoThocTrenBanCo.cpp
@@ -5,7 +5,7 @@ int main() {
 	int soO, sothoc=0;
 	cout << "Nhap so o:"; cin >> soO;
 	for (int i = 0; i < soO; i++) {
-		cout << "O so" << i << "Co so thoc la" <<pow(2,1) << endl;
+		cout << "O so" << i << "Co so thoc la" <<pow(2,i) << endl;
 		sothoc += pow(2,i);
 	}
 	cout << "Tong so thoc chua trong" << soO << "la" << sothoc << "hat\n";
